2f1505c049e06eb9f5ac6e1b5b2b7f36569b83e5
Jim Argeropoulos <tenholeharp@gmail.com>
2016-07-14 23:12:18 -0400

    Starting snapshot

--- test/fakes.js
+++ test/fakes.js
@@ -1,3 +1,15 @@
 (function(){
 	'use strict'
+	const	chai = require('chai'),
+			sut = require('../lib/fakes.js')
+	chai.should()
+
+	describe('fakes:', function(){
+		describe('snapshot:', function(){
+			it('should have a snapshot function', function(){
+				sut.should.have.property('snapshot').a('function')
+			})
+
+		})
+	})
 }())
