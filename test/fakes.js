556febba3dfc9597bd2a14b953b1277746d8543f
Jim Argeropoulos <tenholeharp@gmail.com>
2016-07-15 22:53:06 -0400

    val function implemented

--- test/fakes.js
+++ test/fakes.js
@@ -10,6 +10,23 @@
 				sut.should.have.property('snapshot').a('function')
 			})
 
+			it('should have val function', function(){
+				const snapshot = sut.snapshot({})
+
+				snapshot.should.have.property('val').a('function')
+			})
+
+			it('should return the object passed to snapshot when val is called', function(){
+				const	x = {},
+						snapshot = sut.snapshot(x)
+
+				snapshot.val().should.equal(x)
+			})
+
+			it('should return null if the snapshot is called with undefined', function(){
+				chai.expect(sut.snapshot(undefined).val()).to.be.null
+			})
+
 		})
 	})
 }())
