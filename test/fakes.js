1899fa41c15c6e1fb9073e3ab2f9c77bfc66708e
Jim Argeropoulos <tenholeharp@gmail.com>
2016-07-15 23:05:38 -0400

    Added exists function

--- test/fakes.js
+++ test/fakes.js
@@ -10,23 +10,38 @@
 				sut.should.have.property('snapshot').a('function')
 			})
 
-			it('should have val function', function(){
-				const snapshot = sut.snapshot({})
+			describe('val:', function(){
+				it('should have val function', function(){
+					const snapshot = sut.snapshot({})
 
-				snapshot.should.have.property('val').a('function')
+					snapshot.should.have.property('val').a('function')
+				})
+
+				it('should return the object passed to snapshot when val is called', function(){
+					const	x = {},
+							snapshot = sut.snapshot(x)
+
+					snapshot.val().should.equal(x)
+				})
+
+				it('should return null if the snapshot is called with undefined', function(){
+					chai.expect(sut.snapshot(undefined).val()).to.be.null
+				})
 			})
 
-			it('should return the object passed to snapshot when val is called', function(){
-				const	x = {},
-						snapshot = sut.snapshot(x)
+			describe('expects:', function(){
+				it('should have an exists function', function(){
+					sut.snapshot({}).should.have.property('exists').a('function')
+				})
 
-				snapshot.val().should.equal(x)
+				it('should return true when there is a value passed to snapshot', function(){
+					sut.snapshot({}).exists().should.be.true
+				})
+
+				it('should return false when undefined is passed to snapshot', function(){
+					sut.snapshot(undefined).exists().should.be.false
+				})
 			})
-
-			it('should return null if the snapshot is called with undefined', function(){
-				chai.expect(sut.snapshot(undefined).val()).to.be.null
-			})
-
 		})
 	})
 }())
